5b00bc794c5b210684fa81f19f4b517879c92627
OlikNN <oliknn@gmail.com>
2022-12-14 23:42:13 +0300

    init

---
+++ task_41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+List<int> integer = new List<int>();
+
+string str = "";
+int i = 0;
+int j = 0;
+int count = 0;
+
+while (str != "stop")
+{
+    i++;
+    Console.WriteLine($"Введите число {i}");
+    str = Console.ReadLine();
+
+    if (str != "stop")
+    {
+        integer.Add(Convert.ToInt32(str));
+    }
+}
+
+int[] integerArray = integer.ToArray();
+
+while (j < integerArray.Length)
+{
+    Console.WriteLine($"число {integerArray[j]}");
+
+    if (integerArray[j] > 0)
+    {
+        count++;
+    }
+    j++;
+
+}
+
+Console.WriteLine($"{count} раз пользователь ввел число больше нуля");
---
+++ task_43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите координаты по b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты по k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты по k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double a1 = b2 - b1;
+double a2 = k1 - k2;
+double x = a1 / a2;
+
+Console.WriteLine($"x = {x}");
+
+double a3 = b2 - b1;
+double a4 = k1 - k2;
+
+double y = k1 * a3 / a4 + b1;
+
+Console.WriteLine($"y = {y}");
